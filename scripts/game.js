96d0b6564ca15ca5f590be27dde3760e5d0e32cf
tomdu3 <>
2023-05-03 07:08:30 +0000

    Add newGame function and test for score

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,7 @@ let game = {
     choices: ['button1', 'button2', 'button3', 'button4'],
 };
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+}
+module.exports = { game, newGame };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require('../game');
+const { game, newGame } = require('../game');
 
 beforeAll(() => {
     let fs = require('fs');
@@ -27,5 +27,15 @@ describe('game object contains correct keys', () => {
     });
     test('choices contain correct ids', () => {
         expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
+    });
+});
+
+describe('newGame works correctly', () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test('should set game score to zero', () => {
+        expect(game.score).toEqual(0);
     });
 });
